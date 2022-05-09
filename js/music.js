eccd8abf626e1952f0e805c5232e39aa19d3a164
mhuff17861 <micahhuff1022010@gmail.com>
2022-05-09 14:17:30 -0600

    Upload of initial prototype

---
+++ js/music.js
@@ -0,0 +1,9 @@
+window.onresize = function functionName() {
+  var el = document.getElementById("mainPlayerColumn").offsetHeight;
+  document.getElementById("trackSelectionColumn").style.height = el + "px";
+};
+
+window.onload = function functionName() {
+  var el = document.getElementById("mainPlayerColumn").offsetHeight;
+  document.getElementById("trackSelectionColumn").style.height = el + "px";
+};
