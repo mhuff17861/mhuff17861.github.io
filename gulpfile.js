4222818ef45d215511d842a8bbe3703009a94622
Micah <micahhuff1022010@gmail.com>
2022-05-16 16:52:43 -0600

    Initial setup bootsrap via gulp

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+//import needed plugins
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+//Define pathnames
+const sassSrc = 'scss/bootstrap.scss'
+const sassDest = 'css/'
+const sassInc = [ 'node_modules/bootstrap/scss' ]
+const jsSrc = [ 'node_modules/bootstrap/dist/js/bootstrap.min.js' ]
+const jsDest = 'js/'
+
+function transpileSass() {
+        return gulp.src(sassSrc)
+                .pipe(sass({
+                        includePaths: sassInc
+                }).on('error', sass.logError))
+                .pipe(gulp.dest(sassDest))
+}
+
+function devJS() {
+        return gulp.src(jsSrc)
+                .pipe(gulp.dest(jsDest))
+}
+
+function defaultTask(cb) {
+  // place code for your default task here
+        transpileSass();
+        devJS();
+        cb();
+}
+
+exports.transpileSass = transpileSass
+exports.devJS = devJS
+exports.devSetup = gulp.parallel(transpileSass, devJS)
+exports.default = defaultTask
