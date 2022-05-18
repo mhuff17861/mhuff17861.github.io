e0009fef58fb303a8bcfb1efaa9b851c43625e8d
Micah <micahhuff1022010@gmail.com>
2022-05-18 11:18:09 -0600

    Initial setup template urls complete

--- portfoliosite/resume/urls.py
+++ portfoliosite/resume/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'resume'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('projects', views.projects, name='projects'),
+    path('resume', views.resume, name='resume'),
 ]
--- portfoliosite/resume/views.py
+++ portfoliosite/resume/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'resume/home.html')
+
+def projects(request):
+    return render(request, 'resume/projects.html')
+
+def resume(request):
+    return render(request, 'resume/resume.html')
