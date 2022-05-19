e0315ef096375252a628c7f8cfe555e3dc293e99
Micah <micahhuff1022010@gmail.com>
2022-05-19 15:55:29 -0600

    Missed files like an idiot

--- setup_files/postgre-setup-template.sql
+++ setup_files/postgre-setup-template.sql
@@ -4,3 +4,6 @@ ALTER ROLE portfolioadmin SET client_encoding TO 'utf8';
 ALTER ROLE portfolioadmin SET default_transaction_isolation TO 'read committed';
 ALTER ROLE portfolioadmin SET timezone TO 'EST';
 GRANT ALL PRIVILEGES ON DATABASE portfoliosite TO portfolioadmin;
+
+--Hopefully can get rid of this with project pg_service support later
+ALTER USER portfolioadmin CREATEDB;
