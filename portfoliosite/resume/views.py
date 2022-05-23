c5475212cebbec1237505fe0d57161a78083a475
Micah <micahhuff1022010@gmail.com>
2022-05-23 16:35:58 -0600

    Made card_layout generalized, added annotation to view prep

--- portfoliosite/resume/views.py
+++ portfoliosite/resume/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import F
 from .models import Project
 
 # Create your views here.
 def index(request):
+    card_list = Project.projects.get_projects_by_priority(3).annotate(description=F('short_description'))
     context = {
-        'card_list': Project.projects.get_projects_by_priority(3),
+        'card_list': card_list,
     }
     return render(request, 'resume/home.html', context)
 
